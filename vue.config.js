98b74320c7a278eeed71bd5a417e8a25ecc98a12
PikaPhu Freedom <whitehunter100@gmail.com>
2019-11-20 02:55:05 +0700

    Update config with .env

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,31 @@
 // vue.config.js
+
+// default config var
+const servePort = process.env.SERVEPORT || process.env.npm_config_port || 8080; // serve web at port
+const backend_api = process.env.VUE_APP_API_URL || "http://localhost:7171";
+
+// vue config
 module.exports = {
   // options...
   devServer: {
     disableHostCheck: true,
     proxy: {
-      "^/api": {
-        target: "http://192.168.128.1:7171"
+      "/api": {
+        target: backend_api
+        // changeOrigin: true,
+        // pathRewrite: { '^/api': '' }
+      }
+    },
+    port: servePort
+    //contentBase: './public',
+    /*
+    configureWebpack: config => {
+      if (process.env.NODE_ENV === 'production') {
+        // mutate config for production...
+      } else {
+        // mutate for development...
       }
     }
+    */
   }
 };
