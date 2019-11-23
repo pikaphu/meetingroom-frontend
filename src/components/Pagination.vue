4f81565019e4ee2c1fd336a3bc0a75d355e599b2
PikaPhu Freedom <whitehunter100@gmail.com>
2019-11-24 02:01:56 +0700

    Add Pagination Components

---
+++ src/components/Pagination.vue
@@ -0,0 +1,78 @@
+<template>
+  <nav aria-label="Page navigation example">
+    <ul class="pagination justify-content-end">
+      <li class="page-item" :class="{ disabled: page <= 1 }">
+        <a @click.prevent="onPage(page - 1)" class="page-link" href="#">«</a>
+      </li>
+
+      <li
+        class="page-item"
+        v-for="item in getPageItems"
+        :key="item"
+        :class="{ active: page === item }"
+      >
+        <a @click.prevent="onPage(item)" class="page-link" href="#">{{
+          item
+        }}</a>
+      </li>
+
+      <li class="page-item" :class="{ disabled: page >= getPageItems.length }">
+        <a @click.prevent="onPage(page + 1)" class="page-link" href="#">»</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      page: 1
+    };
+  },
+  props: {
+    data: {
+      required: true
+    }
+    // page: {
+    //   type: Number,
+    //   required: false, // test
+    //   default: 1
+    // }
+  },
+  computed: {
+    getPageItems() {
+      const pages = [];
+      if (this.data) {
+        if (this.data.result && this.data.rows) {
+          const rows = this.data.rows;
+          const limit = this.data.result.length;
+          for (let index = 1; index <= Math.ceil(rows / limit); index++) {
+            pages.push(index);
+          }
+        }
+      }
+      return pages;
+    },
+    getPageItemsFull() {
+      const pages = [];
+      if (this.data) {
+        if (this.data.limit && this.data.rows) {
+          const rows = this.data.rows;
+          const limit = this.data.limit;
+          for (let index = 1; index <= Math.ceil(rows / limit); index++) {
+            pages.push(index);
+          }
+        }
+      }
+      return pages;
+    }
+  },
+  methods: {
+    onPage(page) {
+      this.page = page;
+      this.$emit("onPage", page);
+    }
+  }
+};
+</script>
